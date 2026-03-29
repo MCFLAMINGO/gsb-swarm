ee06ea232adb8a3b50b7eca8a20b56f11ffe432d
MCFLAMINGO <erik@mcflamingo.com>
2026-03-29 18:09:38 -0400

    Update patch-acp.cjs

--- patch-acp.cjs
+++ patch-acp.cjs
@@ -10,35 +10,28 @@ if (!fs.existsSync(acpDistPath)) {
 
 let content = fs.readFileSync(acpDistPath, 'utf8');
 
-// Remove old patch marker so we re-apply cleanly
+// Remove any previous patch markers so we always re-apply cleanly
+content = content.replace(/\/\/ PATCHED BY GSB - START[\s\S]*?\/\/ PATCHED BY GSB - END\n/g, '');
 content = content.replace('// PATCHED BY GSB\n', '');
 
-let patched = false;
+// Fix 1: @account-kit/infra -> viem/chains (chain definitions)
+content = content.replace(
+  'var import_infra = require("@account-kit/infra");',
+  '// PATCHED BY GSB - START\nvar import_infra = require("viem/chains");\n// PATCHED BY GSB - END'
+);
 
-// Fix 1: @account-kit/infra -> viem/chains
-if (content.includes('var import_infra = require("@account-kit/infra");')) {
-  content = content.replace(
-    'var import_infra = require("@account-kit/infra");',
-    'var import_infra = require("viem/chains");'
-  );
-  console.log('[patch-acp] Fixed @account-kit/infra');
-  patched = true;
-}
+// Fix 2: @aa-sdk/core first occurrence -> LocalAccountSigner stub using viem
+const aaSdkStub = `// PATCHED BY GSB - START
+var import_core = { LocalAccountSigner: { privateKeyToAccountSigner: (pk) => { const { privateKeyToAccount } = require('viem/accounts'); const acct = privateKeyToAccount(pk.startsWith('0x') ? pk : '0x'+pk); return { signMessage: async (msg) => acct.signMessage({ message: msg }), getAddress: async () => acct.address, account: acct }; } } };
+// PATCHED BY GSB - END`;
 
-// Fix 2: @aa-sdk/core -> stub
-if (content.includes('var import_aa_sdk_core = require("@aa-sdk/core");')) {
-  content = content.replace(
-    'var import_aa_sdk_core = require("@aa-sdk/core");',
-    'var import_aa_sdk_core = {};'
-  );
-  console.log('[patch-acp] Fixed @aa-sdk/core');
-  patched = true;
-}
+// Fix 3: @aa-sdk/core second occurrence -> reuse import_core
+const aaSdkStub2 = `// PATCHED BY GSB - START
+var import_core2 = import_core;
+// PATCHED BY GSB - END`;
 
-if (patched) {
-  content = '// PATCHED BY GSB\n' + content;
-  fs.writeFileSync(acpDistPath, content, 'utf8');
-  console.log('[patch-acp] All patches applied successfully.');
-} else {
-  console.log('[patch-acp] Nothing to patch.');
-}
+content = content.replace('var import_core = require("@aa-sdk/core");', aaSdkStub);
+content = content.replace('var import_core2 = require("@aa-sdk/core");', aaSdkStub2);
+
+fs.writeFileSync(acpDistPath, content, 'utf8');
+console.log('[patch-acp] All patches applied successfully.');
